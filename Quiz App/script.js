78e52a9f061c90aa4d1aca8863969ec0405cbdb8
katie <kcohen015@gmail.com>
2022-06-07 08:58:24 -0400

    updated-quiz-app

---
+++ Quiz App/script.js
@@ -0,0 +1,44 @@
+//data
+//object that has an array of quizes with question 4 answers and the correct answer
+const quizData = [
+    {
+        question: 'How old is Katie?', 
+        a: '20',
+        b: '25',
+        c: '29',
+        d: '28',
+        correct: 'd'
+    },
+    {
+        question: 'What is the most used programming language?',
+        a: 'Java',
+        b: 'Javascript',
+        c: 'Python',
+        d: 'C',
+        correct: 'a'
+    },
+    {
+        question: 'Who is the US president?',
+        a: 'Trump',
+        b: 'Biden',
+        c: 'Obama',
+        d: 'Mickey Mouse',
+        correct: 'b'
+    },
+    {
+        question: 'What is your anniversary date?',
+        a: 'January 12th, 2018',
+        b: 'February 26th, 2020',
+        c: 'October 20th, 2020',
+        d: 'April 20th, 2018',
+        correct: 'd'
+    },
+    {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Hardward Time Manipulation Language',
+        c: 'Hands Toes Mouth Leftovers',
+        d: 'none of the above',
+        correct: 'a'
+    }
+]
